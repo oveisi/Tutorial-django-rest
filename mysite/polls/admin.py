17d7a9d7f0f318e6df294efc7fa6a3c20898d252
Mohammad Hosein <m.hoseinoveisi@gmail.com>
2018-08-26 16:25:28 +0430

    Remove comments.

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-
 from .models import Question, Choice
-
-# admin.site.register(Choice)
 
 
 class ChoiceInline(admin.TabularInline):
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -19,7 +19,7 @@ class IndexView(generic.ListView):
         """
         # return Question.objects.order_by('-pub_date')[:5]
         return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
-        
+
 
 class DetailView(generic.DetailView):
     model = Question
@@ -36,26 +36,7 @@ class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
-# def index(request):
-#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-#     template = loader.get_template('polls/index.html')
-#     context = {
-#         'latest_question_list' : latest_question_list,
-#     }
-#     return render(request, 'polls/index.html', context)
-#
-# def detail(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     # try:
-#     #     question = Question.objects.get(pk=question_id)
-#     # except Question.DoesNotExist:
-#     #     raise Http404("In Question Nist")
-#     return render(request, 'polls/detail.html', {'question': question})
-#
-# def results(request, question_id):
-#     question = get_object_or_404(Question, pk=question_id)
-#     return render(request, 'polls/results.html', {'question' : question})
-#
+
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
